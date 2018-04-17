27b186707743dbc53e47a2170dc4bcf585d61e55
IIINIII <ibrokhim.shokirov@gmail.com>
2018-04-17 10:07:21 +0300

    First Push
    First Push Ever

---
+++ backend/src/main/java/com/ibrokhim/sprang/SprangApplication.java
@@ -0,0 +1,12 @@
+package com.ibrokhim.sprang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprangApplication.class, args);
+    }
+}
